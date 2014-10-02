d2c436da36786e7f1f8c166a0a8d19486001a9eb
Hackbright Student <info@hackbrightacademy.com>
2014-10-02 16:11:08 -0700

    Replaced third and last element with 37 and 14 errors and counting.

--- list_operations.py
+++ list_operations.py
@@ -65,7 +65,8 @@ def replace_head(input_list):
 
 def replace_third_and_last(input_list):
     """Replace the third and last elements of the input list with the value 37."""
-    pass
+    input_list[2], input_list[-1] = 37, 37
+
 
 def replace_middle(input_list):
     """Replace all elements of the input list with the the values 42 and 37, in
